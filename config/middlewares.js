d71f7f402a7df045e331c263804337839963ceee
Wajiha Hanif Arain <Wajihahanif18@gmail.com>
2025-11-18 01:59:25 +0500

    cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,17 +1,21 @@
 module.exports = [
-  {
-    name: "strapi::cors",
-    config: {
-      origin: ["https://muhammad-bin-hindi-law-firm.vercel.app/"], // Add your Vercel domain here
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeaderOnError: true,
-    },
-  },
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [
+        "https://muhammad-bin-hindi-law-firm.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:1337"
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      credentials: true,
+      keepHeaderOnError: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
